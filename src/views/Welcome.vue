70f7e7b8e8723dc65ed2737e91a8663cf6aed3df
Curtis <curtis@live.fr>
2023-03-12 07:33:00 +0100

    [ADD]: Add files for deployment on heroku

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="welcome">
-    <img
-      src="../assets/logo_white-small.png"
-      alt="logo groupy"
-      width="100px"
-      height="100px"
-    />
+    <img src="../assets/logo_white-small.png" alt="logo groupy" width="100px" height="100px" />
 
     <div class="welcome__title">
       <p>BIENVENUE SUR</p>
-      <h1><span>GROUPY</span> APP</h1>
+      <h1><span>GROUPYY</span> APP</h1>
     </div>
 
     <nav class="welcome__nav">
