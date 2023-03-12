a683226eea765c190d0b4bdfcd6a283bfa186fde
Curtis <curtis@live.fr>
2023-03-12 23:17:54 +0100

    Plug front to api-groupy for heroku deployment

--- src/js/http.js
+++ src/js/http.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api/",
-  // baseURL: "https://groupy-back.herokuapp.com/api/",
+  baseURL: "https://api-groupy.herokuapp.com/api/",
   headers: {
     Authorization: "Bearer " + localStorage.getItem("token"),
   },
