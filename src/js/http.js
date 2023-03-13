ff4e395457b5232a86de45e4424549419f1d2e89
Curtis <curtis@live.fr>
2023-03-13 07:12:23 +0100

    [STYLE]: Change padding of the comment input

--- src/js/http.js
+++ src/js/http.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api-groupy.herokuapp.com/api/",
+  // baseURL: "https://api-groupy.herokuapp.com/api/",
+  baseURL: "http://localhost:3000/api/",
   headers: {
     Authorization: "Bearer " + localStorage.getItem("token"),
   },
