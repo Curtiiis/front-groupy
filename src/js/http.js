215de7b197b4c50418ac96a1e0b07826e3b111c7
Curtis <curtis@live.fr>
2023-03-19 00:33:35 +0100

    [REFACTOR]: improve code syntax

--- src/js/http.js
+++ src/js/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://api-groupy.herokuapp.com/api/",
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://api-groupy.herokuapp.com/api/",
+  // baseURL: "http://localhost:3000/api/",
   headers: {
     Authorization: "Bearer " + localStorage.getItem("token"),
   },
